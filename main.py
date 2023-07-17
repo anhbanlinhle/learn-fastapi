1020002d46e3edd6cdde4e546f3a44584abfeb4f
anhbanlinhle <vietlinhleviet@gmail.com>
2023-07-17 08:55:20 +0700

    request body & post method

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI, Path
+from pydantic import BaseModel
+
+class Item(BaseModel):
+	name: str
+	price: float
+	tax: float = None
+	description: str = None
+	is_offer: bool = None
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
 	return {"message": "Hello World"}
+
+@app.post("/items/")
+async def create_item(item: Item):
+	item_dict = item.dict()
+	if item.tax:
+		price_with_tax = item.price + item.tax
+		item_dict.update({"price_with_tax": price_with_tax})
+	print(item_dict)
+	return item_dict
