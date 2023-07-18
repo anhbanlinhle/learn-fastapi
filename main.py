bd64dad7bb10db95b3c583a4a8a580cf7f31357d
anhbanlinhle <vietlinhleviet@gmail.com>
2023-07-18 16:05:31 +0700

    annotated & query for validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
+from typing import Annotated
 
 class Item(BaseModel):
 	name: str
@@ -23,3 +24,11 @@ async def create_item(item: Item):
 		item_dict.update({"price_with_tax": price_with_tax})
 	print(item_dict)
 	return item_dict
+
+@app.get("/update-item")
+async def update_item(update: Annotated[str | None, Query(max_length = 30)] = None):
+	result = {"item_id": "Foo"}
+	origin = result.copy()
+	if update:
+		result.update({"update": update})
+	return {"origin": origin, "updated": result}
